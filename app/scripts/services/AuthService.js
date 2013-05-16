3c990656d94f7d932558eb0a8ff38cf321764f8f
mranosa <mark.ranosa@irondata.com>
2013-05-17 06:07:55 +0800

    User info is persisted on local storage

--- app/scripts/services/AuthService.js
+++ app/scripts/services/AuthService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .factory('AuthService', function () {
+  .factory('AuthService', function ($location, localStorageService, $rootScope) {
     var USERS_URL = 'https://bayanhero.firebaseIO.com/users';
     var totalUsersRef = new Firebase('https://bayanhero.firebaseIO.com/metrics/total_users');
     var bayanheroRef = new Firebase('https://bayanhero.firebaseIO.com');
@@ -10,6 +10,9 @@ angular.module('miniatureTyrionApp')
     var AuthService = function() {
       authClient = new FirebaseAuthClient(bayanheroRef, function(error, user) {
         if (!error && user) {
+          localStorageService.clearAll();
+          localStorageService.add('user', user);
+          
           var currentUserRef = new Firebase(USERS_URL + '/' + user.id);
           currentUserRef.once('value', function(data) {
             if(!data.val()){
@@ -18,6 +21,10 @@ angular.module('miniatureTyrionApp')
                 return current_value + 1;
               });
             }
+
+            $rootScope.$apply(function(){
+              $location.path('/');  
+            });
           });
         }
       });
