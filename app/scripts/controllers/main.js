22bcdd2c1cc4c7c41159faa9f11a3741859ae960
mranosa <mark.ranosa@irondata.com>
2013-05-17 05:27:27 +0800

    Initial routing for logged in or non logged in user is implemented

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .controller('MainCtrl', function ($scope, localStorageService) {
+  .controller('MainCtrl', function ($scope, localStorageService, $location) {
     
     var userInStore = localStorageService.get('user');
 
     if(userInStore){
     	//TODO go to dashboard
+    	$location.path('/home')
     } else {
     	//TODO go to login
+    	$location.path('/login');
     }
 
   });
