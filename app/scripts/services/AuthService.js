c5c1f59b6c48cf35debb1b636b28a65737690138
mranosa <mark.ranosa@irondata.com>
2013-05-17 05:30:00 +0800

    Initial authservice logic implementation

--- app/scripts/services/AuthService.js
+++ app/scripts/services/AuthService.js
@@ -2,15 +2,32 @@
 
 angular.module('miniatureTyrionApp')
   .factory('AuthService', function () {
-    // Service logic
-    // ...
+    var USERS_URL = 'https://bayanhero.firebaseIO.com/users';
+    var totalUsersRef = new Firebase('https://bayanhero.firebaseIO.com/metrics/total_users');
+    var bayanheroRef = new Firebase('https://bayanhero.firebaseIO.com');
+    var authClient = undefined;
 
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
+    var AuthService = function() {
+      authClient = new FirebaseAuthClient(bayanheroRef, function(error, user) {
+        if (!error && user) {
+          var currentUserRef = new Firebase(USERS_URL + '/' + user.id);
+          currentUserRef.once('value', function(data) {
+            if(!data.val()){
+              currentUserRef.update(user);
+              totalUsersRef.transaction(function(current_value) {
+                return current_value + 1;
+              });
+            }
+          });
+        }
+      });
     };
+
+    AuthService.prototype = {
+      login: function(){
+        authClient.login('facebook');
+      }
+    }
+
+    return new AuthService();
   });
