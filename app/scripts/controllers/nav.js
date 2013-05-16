3b494b93e1d6e6c9d281125af881704cb73cfc06
mranosa <mark.ranosa@irondata.com>
2013-05-17 03:19:38 +0800

    Initial nav controller implementation

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('miniatureTyrionApp')
+  .controller('NavCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
