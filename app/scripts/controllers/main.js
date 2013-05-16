5dfff0712d625e317c815af02e957b47cc1c77da
mranosa <mark.ranosa@irondata.com>
2013-05-17 02:32:06 +0800

    Initial user checking upon load

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    
+    var userInStore = localStorageService.get('user');
+
+    if(userInStore){
+    	//TODO go to dashboard
+    } else {
+    	//TODO go to login
+    }
+
   });
