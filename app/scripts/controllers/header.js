8bcc7bb5b330bb4cb2aac2305090b21828d08d4b
mranosa <mark.ranosa@irondata.com>
2013-05-17 06:56:25 +0800

    Initial header controller implementation

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('miniatureTyrionApp')
+  .controller('HeaderCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
