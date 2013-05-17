2fd6b76f6137c277feeabe558de9b048516cc166
mranosa <mark.ranosa@irondata.com>
2013-05-18 07:40:19 +0800

    Added crud for campaign page

--- app/scripts/controllers/campaigns.js
+++ app/scripts/controllers/campaigns.js
@@ -1,6 +1,48 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .controller('CampaignsCtrl', function ($scope, NavService) {
+  .controller('CampaignsCtrl', function ($scope, NavService, firebaseCollection, firebaseBinding, localStorageService) {
     NavService.updateActiveNav();
+
+    var loggedInUser = localStorageService.get('user');
+    var userDisplayName = localStorageService.get('user_display_name');
+
+    var url = 'https://bayanhero.firebaseIO.com/campaigns';
+    var myUrl = 'https://bayanhero.firebaseIO.com/user_data/' + loggedInUser + '/campaigns';
+    
+    $scope.items = new firebaseCollection(url);
+    $scope.myItems = new firebaseCollection(myUrl);
+
+    $scope.item = {
+		title : '',
+		amount : 0,
+		description : ''
+	}
+
+    $scope.addItem = function(){
+    	var itemUrl = $scope.item.title.toLowerCase().replace(/ /g, '-');
+    	var newItem = {
+				title : $scope.item.title,
+				amount : $scope.item.amount,
+				description : $scope.item.description,
+				currAmount : 0,
+				owner : loggedInUser,
+				userDisplayName : userDisplayName,
+				url : itemUrl
+			};
+
+		$scope.items.$add(newItem, itemUrl);
+		$scope.myItems.$add(newItem, itemUrl);
+
+		$scope.clearItem();
+    };
+
+    $scope.clearItem = function(){
+    	$scope.item = {
+    		title : '',
+    		amount : 0,
+    		description : ''
+    	};
+    };
+    
   });
