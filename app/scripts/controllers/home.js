9952796cb0d19c6df4954d049961bbcf6f298991
mranosa <mark.ranosa@irondata.com>
2013-05-18 07:40:35 +0800

    Home now lists campaigns

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .controller('HomeCtrl', function ($scope, NavService, $rootScope) {
+  .controller('HomeCtrl', function ($scope, NavService, $rootScope, firebaseCollection, firebaseBinding, localStorageService) {
     NavService.updateActiveNav();
     $rootScope.$broadcast('user_logged_in');
+
+    var url = 'https://bayanhero.firebaseIO.com/campaigns';
+    $scope.items = firebaseCollection(url);
+
   });
