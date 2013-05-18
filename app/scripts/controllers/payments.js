79999db8635eab0268f66da3835332cdc1036511
mranosa <mark.ranosa@irondata.com>
2013-05-18 23:37:40 +0800

    Logged in users can view incoming payments

--- app/scripts/controllers/payments.js
+++ app/scripts/controllers/payments.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .controller('PaymentsCtrl', function ($scope, NavService) {
+  .controller('PaymentsCtrl', function ($scope, NavService, firebaseCollection, firebaseBinding, localStorageService) {
     NavService.updateActiveNav();
+
+    var loggedInUser = localStorageService.get('user');
+    var url = 'https://bayanhero.firebaseIO.com/user_data/' + loggedInUser + '/payments';
+    $scope.items = new firebaseCollection(url);
+    
   });
