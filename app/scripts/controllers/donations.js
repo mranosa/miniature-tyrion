1ce81582474725fff5ad0215419c91e4a8153472
mranosa <mark.ranosa@irondata.com>
2013-05-18 23:37:25 +0800

    Logged in user can view their donations

--- app/scripts/controllers/donations.js
+++ app/scripts/controllers/donations.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('miniatureTyrionApp')
-  .controller('DonationsCtrl', function ($scope, NavService) {
+  .controller('DonationsCtrl', function ($scope, NavService, firebaseCollection, firebaseBinding, localStorageService) {
     NavService.updateActiveNav();
+
+    var loggedInUser = localStorageService.get('user');
+    var url = 'https://bayanhero.firebaseIO.com/user_data/' + loggedInUser + '/donations';
+    $scope.items = new firebaseCollection(url);
   });
