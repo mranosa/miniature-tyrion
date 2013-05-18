4a4ed9ca147d9d27377223590cf65082bae7800a
mranosa <mark.ranosa@irondata.com>
2013-05-18 23:37:02 +0800

    Users can now add in their donations

--- app/scripts/controllers/campaignDetail.js
+++ app/scripts/controllers/campaignDetail.js
@@ -4,10 +4,52 @@ angular.module('miniatureTyrionApp')
   .controller('CampaignDetailCtrl', function ($scope, $routeParams, NavService, firebaseCollection, firebaseBinding, localStorageService) {
   	NavService.updateActiveNav();
 	
+	var loggedInUser = localStorageService.get('user');
+    var userDisplayName = localStorageService.get('user_display_name');
 	var campaignUrl = $routeParams.campaignUrl;
 	var url = 'https://bayanhero.firebaseIO.com/campaigns/' + campaignUrl;
 	var promise = firebaseBinding(url, $scope, 'item');
+	$scope.donation = {
+		amount : 0,
+		transactionId : ''
+	};
+
 	promise.then(function(item) {
-		console.log(item);
+		var paymentsUrl = 'https://bayanhero.firebaseIO.com/user_data/' + item.owner + '/payments';
+		var donationsUrl = 'https://bayanhero.firebaseIO.com/user_data/' + loggedInUser + '/donations';
+		
+		$scope.payments = new firebaseCollection(paymentsUrl);
+		$scope.donations = new firebaseCollection(donationsUrl);
+
+		$scope.addItem = function(){
+	    	//add to payments of poster
+	    	$scope.payments.$add({
+	    		amount : $scope.donation.amount,
+	    		transactionId : $scope.donation.transactionId,
+	    		sponsor : loggedInUser,
+	    		sponsorName : userDisplayName,
+	    		url : campaignUrl,
+	    		title : item.title,
+	    		status : 'Pending'
+	    	});
+
+	    	//save to my donation history
+	    	$scope.donations.$add({
+	    		amount : $scope.donation.amount,
+	    		transactionId : $scope.donation.transactionId,
+	    		url : campaignUrl,
+	    		title : item.title,
+	    		status : 'Sent'
+	    	});
+
+			$scope.clearItem();
+	    };
 	});
+
+	$scope.clearItem = function(){
+    	$scope.donation = {
+			amount : 0,
+			transactionId : ''
+		};
+    };
   });
